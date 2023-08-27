1e07dd6008ae280cfbcfe82f16ed88ffef895667
BeaSant0s <107572585+BeaSant0s@users.noreply.github.com>
2023-08-26 23:17:55 -0300

    Update confirmacaocompra.php

--- inicio/sitecompras/confirmacaocompra.php
+++ inicio/sitecompras/confirmacaocompra.php
@@ -13,10 +13,26 @@ session_start();
     <meta http-equiv='X-UA Compatible' content='IE-edge'>
     <title>Document</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" type="text/css" media="screen" href="main-css.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="confirmação.css">
     <script src="main.js"></script>
 </head>
 <body>
-    <div class="confirmacao">CONFIRMAÇÃO DA COMPRA!!!</div>
+   <div class="xain">
+        <h1>XAIN</h1>
+        <p class="rodape1">Tudo | Feminino | Masculino | Infantil</p>
+    </div>
+    <div class="tudo">
+        <div class="confirmacao">CONFIRMAÇÃO DA COMPRA!!!</div>
+    <div class="dados">Seu pedido foi efetuado com sucesso!</br>
+    Para acompanhar seu pedido <a href="">clique aqui</a></div>
+    </div>
+    <div class="rodape2">
+    <p> Informações da empresa </br>
+       Sobre XAIN </br>
+       Ajuda e suporte</br>
+       Devolução</br>
+       nossas redes: @lojasxain_ </br>
+</p>
+</div>
 </body>
 </html>
