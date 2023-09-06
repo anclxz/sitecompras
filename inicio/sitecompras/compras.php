c9ff6003ca612cfca5070d8993596a0ff834c437
BeaSant0s <107572585+BeaSant0s@users.noreply.github.com>
2023-09-06 10:11:46 -0300

    Update compras.php

--- inicio/sitecompras/compras.php
+++ inicio/sitecompras/compras.php
@@ -63,70 +63,70 @@ if(isset($_POST['comprar'])){
             <td><input type="checkbox" name="ch0"></td>
             <td><input type="text" name="desc0" value="Short desporto" readonly></td>
             <td><input type="numb" name="qtd0" value="0"></td>
-            <td><input type="text" name="vl0" value="75.00" readonly></td>
+            <td><input type="text" name="vl0" value="R$ 75,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="blusa.jpg" alt="blusa star wars"></td>
             <td><input type="checkbox" name="ch1"></td>
             <td><input type="text" name="desc1" value="blusa star wars" readonly></td>
             <td><input type="numb" name="qtd1" value="0"></td>
-            <td><input type="text" name="vl1" value="50.00" readonly></td>
+            <td><input type="text" name="vl1" value="R$ 50,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="tenis.jpeg" alt="tenis nike"></td>
             <td><input type="checkbox" name="ch2"></td>
             <td><input type="text" name="desc2" value="Tenis Nike" readonly></td>
             <td><input type="numb" name="qtd2" value="0"></td>
-            <td><input type="text" name="vl2" value="399.00" readonly></td>
+            <td><input type="text" name="vl2" value="R$ 399,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="bolsa.jpg" alt="bolsa feminina"></td>
             <td><input type="checkbox" name="ch3"></td>
             <td><input type="text" name="desc3" value="bolsa feminina" readonly></td>
             <td><input type="numb" name="qtd3" value="0"></td>
-            <td><input type="text" name="vl3" value="100.00" readonly></td>
+            <td><input type="text" name="vl3" value="R$ 100,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="brincos.jpg" alt="brincos ying yang"></td>
             <td><input type="checkbox" name="ch4"></td>
             <td><input type="text" name="desc4" value="brincos ying yang" readonly></td>
             <td><input type="numb" name="qtd4" value="0"></td>
-            <td><input type="text" name="vl4" value="20.00" readonly></td>
+            <td><input type="text" name="vl4" value="R$ 20,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="meias.jpg" alt="meias stitch"></td>
             <td><input type="checkbox" name="ch5"></td>
             <td><input type="text" name="desc5" value="meias stitch" readonly></td>
             <td><input type="numb" name="qtd5" value="0"></td>
-            <td><input type="text" name="vl5" value="35.00" readonly></td>
+            <td><input type="text" name="vl5" value="R$ 35,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="carteira.jpg" alt="carteira feminina"></td>
             <td><input type="checkbox" name="ch6"></td>
             <td><input type="text" name="desc6" value="carteira feminina" readonly></td>
             <td><input type="numb" name="qtd6" value="0"></td>
-            <td><input type="text" name="vl6" value="85.00" readonly></td>
+            <td><input type="text" name="vl6" value="R$ 85,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="anel.jpg" alt="anel olho grego"></td>
             <td><input type="checkbox" name="ch7"></td>
             <td><input type="text" name="desc7" value="anel olho grego" readonly></td>
             <td><input type="numb" name="qtd7" value="0"></td>
-            <td><input type="text" name="vl7" value="50.00" readonly></td>
+            <td><input type="text" name="vl7" value="R$ 50,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="cropped.jpg" alt="cropped borboleta"></td>
             <td><input type="checkbox" name="ch8"></td>
             <td><input type="text" name="desc8" value="cropped borboleta" readonly></td>
             <td><input type="numb" name="qtd8" value="0"></td>
-            <td><input type="text" name="vl8" value="39.00" readonly></td>
+            <td><input type="text" name="vl8" value="R$ 39,00" readonly></td>
         </tr>
         <tr>
             <td><img class="img" src="vestido.jpg" alt="vestido quadriculado"></td>
             <td><input type="checkbox" name="ch9"></td>
             <td><input type="text" name="desc9" value="vestido quadriculado" readonly></td>
             <td><input type="numb" name="qtd9" value="0"></td>
-            <td><input type="text" name="vl9" value="95.00" readonly></td>
+            <td><input type="text" name="vl9" value="R$ 95,00" readonly></td>
         </tr>
         <tr>
             <td colspan="5">
